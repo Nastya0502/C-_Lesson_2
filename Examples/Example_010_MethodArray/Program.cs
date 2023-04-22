b96d0a6d212cb789264740c4f49c75b432c2c8a3
Anastasia Selenchik <nastenaselenchik@gmail.com>
2023-04-22 18:25:10 +0300

    Example 10

---
+++ Examples/Example_010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {25,6,8,99,63,45,85};
+int n = array.Length;
+int find = 99;
+int index = 0;
+while (index<n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
